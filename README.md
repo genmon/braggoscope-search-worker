f363ac28d6add958e6b2095e5313a3ece5b716ff
Matt Webb <matt@interconnected.org>
2024-12-14 20:33:18 +0000

    Skeleton

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # braggoscope-search-worker
+
 Braggoscope search backend on a Cloudflare worker
+
+## Setup
+
+```bash
+npx wrangler vectorize create braggoscope-index --preset @cf/baai/bge-base-en-v1.5
+```
+
+## Deploy
+
+```bash
+npx wrangler deploy
+```
+
+## Test
+
+```bash
+curl --json '{"query": "hello"}' https://braggoscope-search-worker.genmon.workers.dev/search
+```
--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,57 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	VECTORIZE: Vectorize;
+	AI: Ai;
+}
+
+interface EmbeddingResponse {
+	shape: number[];
+	data: number[][];
+}
+
+const CORS = {
+	'Access-Control-Allow-Origin': '*',
+	'Access-Control-Allow-Methods': 'POST',
+	'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+};
+
+type Episode = {
+	id: string;
+	title: string;
+	published: string;
+	permalink: string;
+	description: string;
+};
+
+type Found = Omit<Episode, 'description'> & { score: number };
+
+async function getEpisodes(): Promise<Episode[]> {
+	return await fetch('https://www.braggoscope.com/episodes.json').then((res) => res.json());
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+		const method = request.method;
+		const path = new URL(request.url).pathname;
+
+		if (method === 'GET') {
+			if (path.startsWith('/favicon')) {
+				return new Response('', { status: 404 });
+			} else {
+				return new Response('Not Found', { status: 404 });
+			}
+		} else if (method === 'POST') {
+			if (path.startsWith('/search')) {
+				return new Response('{}', { headers: CORS });
+			} else {
+				return new Response('Not Found', { status: 404 });
+			}
+		} else if (method === 'OPTIONS') {
+			return new Response('OK', { headers: CORS });
+		}
+
+		return new Response('Method Not Allowed', { status: 405 });
 	},
 } satisfies ExportedHandler<Env>;
